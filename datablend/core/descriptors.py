8fb533c2930140c140e2ec831982c3f6cf6dde39
Imperial College <cbit@Imperials-MacBook-Pro-2.local>
2020-12-28 13:50:36 +0100

    - deleted

--- datablend/core/descriptors.py
+++ datablend/core/descriptors.py
@@ -1,17 +1,45 @@
 # Libraries
 import re
 import ast
+import yaml
 import json
+import logging
 import numpy as np
 import pandas as pd
+import logging.config
+
+# Specific
+from pathlib import Path
 
 # Own libraries
+from datablend.utils.pandas import save_xlsx
 from datablend.utils.merge import merge_date_time
 from datablend.utils.transformations import convert_dtypes_categorical
 from datablend.utils.transformations import convert_dtypes_datetime
 from datablend.utils.transformations import format_var_names
 from datablend.utils.transformations import str2eval
 
+from datablend.core.widgets import RenameWidget
+from datablend.core.widgets import ReplaceWidget
+from datablend.core.widgets import DateTimeMergeWidget
+from datablend.core.widgets import TemplateTransformWidget
+
+from datablend.utils.compute import datetime
+
+"""
+# ------------------------------------------
+# Constants
+# ------------------------------------------
+# Load logging configuration
+with open('./logging.yaml', 'r') as stream:
+    config = yaml.load(stream, Loader=yaml.FullLoader)
+
+# Config logging from file.
+logging.config.dictConfig(config)
+
+# Create logger
+logger = logging.getLogger('dev')
+"""
 
 
 def create_descriptor_list(l):
@@ -42,7 +70,8 @@ class ColumnDescriptor:
         self.to_replace = getattr(self, 'to_replace', None)
         self.datetime = getattr(self, 'datetime', False)
         self.datetime_date = getattr(self, 'datetime_date', None)
-        self.datetime_time = getattr(self, 'datetime_time', None)
+        self.datetime_time = getattr(self, 'datetime_time', None)  # automatizar esto!
+        self.unit = getattr(self, 'unit', None)
 
         # Format to_name
         if self.to_name is None:
@@ -59,7 +88,7 @@ class ColumnDescriptor:
         return series
 
     def show(self):
-        for k,v in self.__dict__.items():
+        for k, v in self.__dict__.items():
             print("{0}: {1}".format(k, v), end=' | ')
         print("")
 
@@ -116,22 +145,73 @@ def category_maps(data, max_nunique=5):
     return d
 
 
-class DatasetDescriptor:
-    """This class describes the dataset transformations.
+class Template:
+    """Description...
 
-    The aim of this class is to"""
 
-    descriptors = []
+    from_name:
 
-    def __init__(self, filepath=None):
-        """The constructor."""
+    to_name:
+
+    type:
+
+    to_replace:
+
+    datetime:
+
+    datetime_date:
+
+    datetime_time:
+
+    unit:
+
+    """
+    # Dataframe
+    df = None
+
+    def __init__(self):
         pass
 
-    def infer_cfg_file(self, data):
+    def has_timestamp(self):
+        return not ~self.df.timestamp.isnull().all()
+
+    def check(self):
+        pass
+
+    def fit_from_template(self, template):
+        """Fits the resources_artificial.
+
+        Parameters
+        ----------
+        template: pd.DataFrame
+            The dataframe with the resources_artificial information.
+
+        Returns
+        -------
+        Template object
+        """
+        # Check the resources_artificial is valid.
+
+        # Convert to_replace to dict
+        template.to_replace = template.to_replace.apply(str2eval)
+
+        # Set resources_artificial
+        self.df = template
+        # Return
+        return self
+
+    def fit_from_data(self, data):
         """Creates a configuration file for the descriptor.
 
-        The result is just an approximation and needs further inspection
-        by the user to ensure that all the transformations are correct.
+        .. todo: Review the way the category maps are created.
+                 For instance rename categories when they have
+                 been encoded as numbers, but if they are
+                 strings leave them and don't create automatically
+                 a to_replace?
+
+        The result produced is just a resources_artificial and needs further inspection
+        by the user to ensure that all the transformations and parameters
+        are correct.
 
         Parameters
         ----------
@@ -154,7 +234,8 @@ class DatasetDescriptor:
                                      'timestamp',
                                      'datetime',
                                      'datetime_date',
-                                     'datetime_time'])
+                                     'datetime_time',
+                                     'unit'])
 
         # Fill basic configuration DataFrame
         ccfg.from_name = data.columns
@@ -173,8 +254,104 @@ class DatasetDescriptor:
         # Reset index and keep name in column
         ccfg = ccfg.rename_axis('from_name').reset_index()
 
+        # Set
+        self.df = ccfg
+
         # Return
-        return ccfg
+        return self
+
+    def transform(self, data):
+        """This method...
+
+        .. todo: raise warnings columns in data are not in resources_artificial
+        .. todo: raise warnings columns in temp are not in data
+        .. todo: convert merge_date_time as widget?
+        """
+        # Copy data
+        df = data.copy(deep=True)
+
+        # Merge date/time columns.
+        df = DateTimeMergeWidget(self.df).transform(df)
+
+        # Rename column names.
+        df = RenameWidget(self.df).transform(df)
+
+        # Replace column values.
+        df = ReplaceWidget(self.df).transform(df)
+
+        # Template transform
+        df = TemplateTransformWidget(self.df).transform(df)
+
+        # Return
+        return df
+
+    def __str__(self):
+        return str(self.df)
+
+
+def Blender():
+    """This method..."""
+
+
+class DataBlender:
+    """This class helps with the descriptor templates.
+
+    Explanation in detail.
+
+    """
+    templates = {}
+
+    def fit(self, data):
+        """Creates a configuration file for the data file.
+
+        The result produced is just a resources_artificial and needs further inspection
+        by the user to ensure that all the transformations and parameters
+        are correct.
+
+        Parameters
+        ----------
+        filepath: str-like
+            The full data filepath (supported .csv, .xls, .xlsx and .json)
+
+        **kwargs:
+            The parameters needed to read files using pandas.
+
+        Returns
+        --------
+        """
+        # Create dictionary
+        if not isinstance(data, dict):
+            data = {'ROOT': data}
+
+        # For each sheet create resources_artificial
+        for k, df in data.items():
+            self.templates[k] = Template().fit_from_dataframe(df)
+
+        # Return
+        return self.templates
+
+
+
+
+class DBDescriptor:
+    """This class describes the dataset transformations.
+
+    The aim of this class is to"""
+
+
+
+    descriptors = []
+
+    def __init__(self, verbose=10):
+        """The constructor."""
+        pass
+
+    def fit_dataframe(self, ccfg):
+        """Fits from dataframe"""
+        ccfg = ccfg.to_json(orient='records')
+        ccfg = json.loads(ccfg)
+        self.descriptors = create_descriptor_list(ccfg)
+        return self
 
     def fit_csv(self, filepath):
         """Fit DatasetDescriptor from csv file.
@@ -263,15 +440,17 @@ class DatasetDescriptor:
         for c in self.descriptors:
             if c.to_name not in df.columns:
                 if (c.datetime is not None) and \
-                   (c.datetime_date is not None) and \
-                   (c.datetime_time is not None):
+                        (c.datetime_date is not None) and \
+                        (c.datetime_time is not None):
+                    # df[c.to_name] = df.apply(lambda x: datetime(x[c.datetime_date],
+                    #                                            x[c.datetime_time]), axis=1)
                     df[c.to_name] = merge_date_time(df, c.datetime_date, c.datetime_time)
-                    df = df.drop(labels=[c.datetime_date,
-                                         c.datetime_time], axis=1)
+                    # df = df.drop(labels=[c.datetime_date,
+                    #                     c.datetime_time], axis=1)
 
         # Rename column names.
         df = df.rename(columns=
-            {c.from_name: c.to_name for c in self.descriptors})
+                       {c.from_name: c.to_name for c in self.descriptors})
 
         # Replace column values. Note that the values in the
         # config file contain first the string value and then the
@@ -279,22 +458,34 @@ class DatasetDescriptor:
         #
         # Example: {"Female": 2, "Male": 1}
         df = df.replace({c.to_name:
-            {v: k for k, v in c.to_replace.items()}
-                for c in self.descriptors
-                    if c.to_replace is not None})
+                             {v: k for k, v in c.to_replace.items()}
+                         for c in self.descriptors
+                         if c.to_replace is not None})
 
         # Format columns using descriptors
         for c in self.descriptors:
             if c.to_name in df.columns:
                 df[c.to_name] = c.transform(df[c.to_name])
 
+        # Set date as an event.
+        for c in self.descriptors:
+            if c.to_name == c.timestamp:
+                df['%s' % c.to_name.split('_')[-1]] = ~pd.isnull(df[c.to_name])
+
         # Return
         return df
-    
+
     def get_timestamp_feature_tuples(self):
         """This method..."""
         l = []
         for c in self.descriptors:
             if c.timestamp is not None:
-                l.append((c.timestamp, c.to_name))
+                if c.to_name == c.timestamp:
+                    l.append((c.timestamp, '%s' % c.to_name.split('_')[-1]))
+                else:
+                    l.append((c.timestamp, c.to_name))
         return l
+
+    def get_units(self):
+        return [[c.to_name, c.unit] for c in self.descriptors
+                if c.unit is not None]
