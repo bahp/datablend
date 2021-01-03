f7694369e887f897ac0798a1456e660fa3c93f40
Imperial College <cbit@Imperials-MacBook-Pro-2.local>
2021-01-03 17:55:00 +0100

    + oucru-full example

--- examples/oucru/oucru-full/automated_run.sh
+++ examples/oucru/oucru-full/automated_run.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
 # Execute 06dx
-python ../oucru-06dx/create_data_fixed.py
-python ../oucru-06dx/create_data_stacked.py
-python ../oucru-06dx/create_data_tidy.py
+python ../oucru-06dx/create_data_fixed.py || exit
+python ../oucru-06dx/create_data_stacked.py || exit
+python ../oucru-06dx/create_data_tidy.py || exit
 
 # Execute 13dx
-python ../oucru-13dx/create_data_fixed.py
-python ../oucru-13dx/create_data_stacked.py
-python ../oucru-13dx/create_data_tidy.py
+python ../oucru-13dx/create_data_fixed.py || exit
+python ../oucru-13dx/create_data_stacked.py || exit
+python ../oucru-13dx/create_data_tidy.py || exit
 
 # Execute 32dx
-python ../oucru-32dx/create_data_fixed.py
-python ../oucru-32dx/create_data_stacked.py
-python ../oucru-32dx/create_data_tidy.py
+python ../oucru-32dx/create_data_fixed.py || exit
+python ../oucru-32dx/create_data_stacked.py || exit
+python ../oucru-32dx/create_data_tidy.py || exit
 
 # Execute 42dx
-python ../oucru-42dx/create_data_fixed.py
-python ../oucru-42dx/create_data_stacked.py
-python ../oucru-42dx/create_data_tidy.py
+python ../oucru-42dx/create_data_fixed.py || exit
+python ../oucru-42dx/create_data_stacked.py || exit
+python ../oucru-42dx/create_data_tidy.py || exit
 
 # Execute combine data tidy
-python combine_data_tidy.py
+python combine_data_tidy.py || exit
 
 # Copy
 cp ../oucru-06dx/resources/outputs/datasets/06dx_data_tidy.csv ./resources/datasets/
--- examples/oucru/oucru-full/combine_data_tidy.py
+++ examples/oucru/oucru-full/combine_data_tidy.py
@@ -22,14 +22,18 @@ pd.set_option("display.max_rows", None,
 path_data = './resources/datasets'
 outp_path = './resources/outputs/datasets'
 
-# Create path
-p = Path(path_data)
+
+# Path with raw data.
+path_data = Path('./resources/datasets')
+
+# Path to save fixed data.
+path_comb = Path('./resources/datasets')
 
 # Create empty data
 data = pd.DataFrame()
 
 # Read all files
-for path in list(p.glob('**/*.csv')):
+for path in sorted(list(path_data.glob('**/*.csv'))):
     aux = pd.read_csv(path)       # Read data
     print(str_dtypes(aux, path))  # Show dtypes
     data = data.append(aux)       # Append
@@ -41,9 +45,21 @@ print(str_dtypes(data, 'Combined'))
 print(str_dtypes(settings.prefix_sources(data)))
 
 # Show sizes
-for path in list(p.glob('**/*.csv')):
+for path in sorted(list(path_data.glob('**/*.csv'))):
     aux = pd.read_csv(path)
     profiles = aux.shape
     patients = aux.study_no.nunique()
     print('%s: Patients %5s | Days %5s | Features %3s' %
           (path.name, patients, profiles[0], profiles[1]))
+
+# -----------------
+# Save
+# -----------------
+# Create file name
+tidy_files = list(path_data.glob('**/*.csv'))
+tidy_ids = sorted([e.stem.split("_")[0] for e in tidy_files])
+filename = '{0}_data_tidy.csv'.format('_'.join(tidy_ids))
+filepath = '{0}/{1}'.format(path_comb, filename)
+
+# Save
+data.to_csv(filepath, index=False)
