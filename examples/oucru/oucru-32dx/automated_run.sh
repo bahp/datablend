f66ce3735407a3ce5560b8b361338c3a13f55466
Imperial College <cbit@dyn3130-90.wlan.ic.ac.uk>
2021-02-22 12:11:48 +0000

    + oucru-32dx example

--- examples/oucru/oucru-32dx/automated_run.sh
+++ examples/oucru/oucru-32dx/automated_run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # 32dx
-python create_data_fixed.py
-python create_data_stacked.py
-python create_data_tidy.py
+python create_data_fixed.py || exit
+python create_data_stacked.py || exit
+python create_data_tidy.py || exit
